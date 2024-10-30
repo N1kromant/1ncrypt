588123de5c8ccbfd6301bcb02dce7ea1b45d9736
N1kromant <nikromka@gmail.com>
2024-10-30 06:18:59 +0600

    Task is ready

--- src/EncrytionModule.rs
+++ src/EncrytionModule.rs
@@ -1,4 +1,9 @@
-pub fn blockEncrypt(data: &[u8], key: &[u8]) -> Vec<u8> {
+#![allow(non_snake_case)]
+use rand::Rng;
+
+pub static BLOCKSIZE: usize = 4;
+
+pub fn blockXor(data: &[u8], key: &[u8]) -> Vec<u8> {
     // Создаем новый вектор для хранения результата
     let mut result = Vec::with_capacity(data.len());
 
@@ -12,4 +17,35 @@ pub fn blockEncrypt(data: &[u8], key: &[u8]) -> Vec<u8> {
     }
 
     result
+}
+
+pub fn encrypt(data: &mut [u8], key: &mut [u8]) -> Vec<u8> {
+    let VI: Vec<u8> = getVI();
+    let mut result: Vec<u8> = vec![];
+    let mut lastEnc: Vec<u8> = vec![];
+    let mut thisEnc: Vec<u8> = vec![];
+
+    result.extend(VI.clone());
+
+    for (i, block) in data.chunks_mut(BLOCKSIZE).enumerate() {
+        if i == 0 {
+            blockXor(block, VI.as_slice());
+            thisEnc = blockXor(block, key);
+        } else {
+            blockXor(block, &lastEnc);
+            thisEnc = blockXor(block, key);
+        }
+
+        result.extend(blockXor(block, key));
+        lastEnc = thisEnc;
+    }
+
+    return result;
+}
+
+pub fn getVI() -> Vec<u8>{
+    let mut rng = rand::thread_rng();
+    
+    let random_bytes: Vec<u8> = (0..4).map(|_| rng.gen()).collect();
+    return random_bytes;
 }
--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,36 @@
 use std::io;
 mod MutateFileModule;
 mod EncrytionModule;
+
 fn main() {
     let outFileName = "f.wut";
     let keyFileName = "yek.key";
 
-    let filedata = MutateFileModule::readFile(keyFileName).expect("NODATA");
+    let mut key = MutateFileModule::readFile(keyFileName).expect("NODATA");
     let mut input = String::new();
+
+    let iv:Vec<u8> = vec![]; 
 
     io::stdin()
         .read_line(&mut input)
         .expect("Не удалось прочитать строку");
-    let input = input.trim();
+    let mut input = input.trim();
 
-    let filedataBin: String = filedata.as_bytes().iter()
+    let keyBin: String = key.as_bytes().to_vec().iter()
         .map(|byte| byte.to_string())
         .collect::<Vec<String>>()
         .join(" ");
-    let inputBin: String = input.as_bytes().iter()
+    let inputBin: String = input.as_bytes().to_vec().iter()
         .map(|byte| byte.to_string())
         .collect::<Vec<String>>()
         .join(" ");
 
-    println!("inputBin:{}", inputBin);
-    println!("filedataBin:{}", filedataBin);
-
-    let encrypted = EncrytionModule::blockEncrypt(input.as_bytes(), filedata.as_bytes());
+    let mut encrypted = EncrytionModule::encrypt(&mut input.as_bytes().to_vec(), &mut key.as_bytes().to_vec());
 
     let encryptedBin = encrypted.iter()
     .map(|byte| byte.to_string())
     .collect::<Vec<String>>()
     .join(" ");
-
-    println!("encoded:{}", encryptedBin);
 
     MutateFileModule::writeFile(outFileName, unsafe { &String::from_utf8_unchecked(encrypted)});
 
